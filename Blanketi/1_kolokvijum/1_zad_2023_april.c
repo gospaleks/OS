845751385d201b1e4fae1b43da14301bd2a595f8
bxgda <jovan.bogdanovic@elfak.rs>
2024-11-21 00:48:51 +0100

    bogda sesija 2

---
+++ Blanketi/1_kolokvijum/1_zad_2023_april.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+/* uvek je veci od 2000 koliko sam ja testirao */
+
+#define N 10
+#define M 10
+
+int niz[N];
+int zbirNiza;
+
+sem_t upisParna, upisNeparna, provera;
+
+void *parnaMesta(void *arg)
+{
+    for (int i = 0; i < M; i++)
+    {
+        sem_wait(&upisParna);
+
+        for (int j = 0; j < N; j += 2)
+            niz[j] = rand() % 300;
+
+        sem_post(&provera);
+    }
+}
+
+void *neparnaMesta(void *arg)
+{
+    for (int i = 0; i < M; i++)
+    {
+        sem_wait(&upisNeparna);
+
+        for (int j = 1; j < N; j += 2)
+            niz[j] = (rand() % 200) + 300;
+
+        sem_post(&provera);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    sem_init(&upisParna, 0, 1);
+    sem_init(&upisNeparna, 0, 1);
+    sem_init(&provera, 0, 0);
+
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, (void *)parnaMesta, NULL);
+    pthread_create(&t2, NULL, (void *)neparnaMesta, NULL);
+
+    for (int i = 0; i < M; i++)
+    {
+        sem_wait(&provera);
+        sem_wait(&provera);
+
+        zbirNiza = 0;
+        for (int j = 0; j < N; j++)
+            zbirNiza += niz[j];
+
+        if (zbirNiza > 2000)
+            printf("zbirNiza: %d, zbir niza je VECI od 2000\n", zbirNiza);
+        else
+            printf("zbirNiza: %d, zbir niza je MANJI od 2000\n", zbirNiza);
+
+        sem_post(&upisParna);
+        sem_post(&upisNeparna);
+    }
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&upisNeparna);
+    sem_destroy(&upisParna);
+    sem_destroy(&provera);
+
+    return 0;
+}
