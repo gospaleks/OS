7f409babf01be37b045263d01df3f154893178a4
bxgda <jovan.bogdanovic@elfak.rs>
2024-11-21 19:16:00 +0100

    2 semafora

--- Blanketi/1_kolokvijum/1_zad_2023_april.c
+++ Blanketi/1_kolokvijum/1_zad_2023_april.c
@@ -6,18 +6,18 @@
 /* uvek je veci od 2000 koliko sam ja testirao */
 
 #define N 10
-#define M 5
+#define M 10
 
 int niz[N];
 int zbirNiza;
 
-sem_t upisParna, upisNeparna, provera;
+sem_t upis, provera;
 
 void *parnaMesta(void *arg)
 {
     for (int i = 0; i < M; i++)
     {
-        sem_wait(&upisParna);
+        sem_wait(&upis);
 
         for (int j = 0; j < N; j += 2)
             niz[j] = rand() % 300;
@@ -31,7 +31,7 @@ void *neparnaMesta(void *arg)
 {
     for (int i = 0; i < M; i++)
     {
-        sem_wait(&upisNeparna);
+        sem_wait(&upis);
 
         for (int j = 1; j < N; j += 2)
             niz[j] = (rand() % 200) + 300;
@@ -45,8 +45,7 @@ int main()
 {
     srand(time(NULL));
 
-    sem_init(&upisParna, 0, 1);
-    sem_init(&upisNeparna, 0, 1);
+    sem_init(&upis, 0, 2);
     sem_init(&provera, 0, 0);
 
     pthread_t t1, t2;
@@ -68,15 +67,14 @@ int main()
         else
             printf("zbirNiza: %d, zbir niza je MANJI od 2000\n", zbirNiza);
 
-        sem_post(&upisParna);
-        sem_post(&upisNeparna);
+        sem_post(&upis);
+        sem_post(&upis);
     }
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
-    sem_destroy(&upisNeparna);
-    sem_destroy(&upisParna);
+    sem_destroy(&upis);
     sem_destroy(&provera);
 
     return 0;
