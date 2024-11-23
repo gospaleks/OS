dbebc4af8fd9ae3f78071c7b5fe3a312913bda6d
gospaleks <aleksandar.gospavic@elfak.rs>
2024-11-23 01:26:22 +0100

    lab 4 v2

--- Lab/Lab 4/Zad_2/zad2.c
+++ Lab/Lab 4/Zad_2/zad2.c
@@ -1,87 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	// Da pregazi prethodno stanje pre nego pocne da appenduje
-	FILE *f = fopen("tmp.txt", "w");
-	if (f == 0)
-		exit(1);
-	fclose(f);
+    // Da pregazi prethodno stanje pre nego pocne da appenduje
+    FILE *f = fopen("tmp.txt", "w");
+    if (f == 0)
+        exit(1);
+    fclose(f);
 
-	// Kreiranje deskriptora
-	int pd[2];
-	if (pipe(pd) < 0)
-		exit(1);
+    // Kreiranje deskriptora
+    int pd[2];
+    if (pipe(pd) < 0)
+        exit(1);
 
-	int *niz;
-	int n;
+    int *niz;
+    int n;
 
-	int pid = fork();
-	if (pid == 0)
-	{
-		close(pd[1]); // Zatvori kraj za pisanje
+    int pid = fork();
+    if (pid == 0)
+    {
+        close(pd[1]); // Zatvori kraj za pisanje
 
-		while (1)
-		{
-			read(pd[0], &n, sizeof(int));
+        while (1)
+        {
+            read(pd[0], &n, sizeof(int));
 
-			if (n == -1)
-			{
-				close(pd[0]);
-				break;
-			}
+            if (n == -1)
+            {
+                close(pd[0]);
+                break;
+            }
 
-			FILE *f = fopen("tmp.txt", "a");
-			if (f == 0)
-				exit(1);
+            FILE *f = fopen("tmp.txt", "a");
+            if (f == 0)
+                exit(1);
 
-			int procitaniBroj;
-			for (int i = 0; i < n; ++i)
-			{
-				read(pd[0], &procitaniBroj, sizeof(int));
-				fprintf(f, "%d\n", procitaniBroj);
-			}
+            int procitaniBroj;
+            for (int i = 0; i < n; ++i)
+            {
+                read(pd[0], &procitaniBroj, sizeof(int));
+                fprintf(f, "%d\n", procitaniBroj);
+            }
 
-			fclose(f);
-		}
-	}
-	else
-	{
-		close(pd[0]); // Zatvori kraj za citanje
+            fclose(f);
+        }
+    }
+    else
+    {
+        close(pd[0]); // Zatvori kraj za citanje
 
-		while (1)
-		{
-			printf("Unesite N: ");
-			scanf("%d", &n);
+        while (1)
+        {
+            printf("Unesite N: ");
+            scanf("%d", &n);
 
-			write(pd[1], &n, sizeof(int));
+            write(pd[1], &n, sizeof(int));
 
-			if (n == -1)
-			{
-				close(pd[1]);
-				break;
-			}
+            if (n == -1)
+            {
+                close(pd[1]);
+                break;
+            }
 
-			// Generisanje random brojeva
-			niz = (int *)malloc(sizeof(int) * n);
-			for (int i = 0; i < n; ++i)
-				niz[i] = rand() % 101;
+            // Generisanje random brojeva
+            niz = (int *)malloc(sizeof(int) * n);
+            for (int i = 0; i < n; ++i)
+                niz[i] = rand() % 101;
 
-			for (int i = 0; i < n; ++i)
-				write(pd[1], &niz[i], sizeof(int));
+            for (int i = 0; i < n; ++i)
+                write(pd[1], &niz[i], sizeof(int));
 
-			free(niz);
-		}
+            free(niz);
+        }
 
-		wait(NULL);
-	}
+        wait(NULL);
+    }
 
-	return 0;
+    return 0;
 }
--- Lab/Lab 4/Zad_4/zad4.c
+++ Lab/Lab 4/Zad_4/zad4.c
@@ -1,57 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
 #define LEN 100
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-	{
-		printf("Neodgovarajuci broj argumenata.\n");
-		exit(1);
-	}
+    if (argc != 3)
+    {
+        printf("Neodgovarajuci broj argumenata.\n");
+        exit(1);
+    }
 
-	char buff[LEN];
+    char buff[LEN];
 
-	int pd[2];
-	if (pipe(pd) < 0)
-		exit(1);
+    int pd[2];
+    if (pipe(pd) < 0)
+        exit(1);
 
-	int pid = fork();
-	if (pid == 0)
-	{
-		close(pd[1]);
+    int pid = fork();
+    if (pid == 0)
+    {
+        close(pd[1]);
 
-		FILE *f = fopen(argv[2], "w");
-		if (f == 0)
-			exit(1);
+        FILE *f = fopen(argv[2], "w");
+        if (f == 0)
+            exit(1);
 
-		while (read(pd[0], buff, LEN))
-			fprintf(f, "%s", buff);
+        while (read(pd[0], buff, LEN))
+            fprintf(f, "%s", buff);
 
-		fclose(f);
-		close(pd[0]);
-	}
-	else
-	{
-		close(pd[0]);
+        fclose(f);
+        close(pd[0]);
+    }
+    else
+    {
+        close(pd[0]);
 
-		FILE *f = fopen(argv[1], "r");
-		if (f == 0)
-			exit(1);
+        FILE *f = fopen(argv[1], "r");
+        if (f == 0)
+            exit(1);
 
-		while (fgets(buff, LEN, f))
-			write(pd[1], buff, LEN);
+        while (fgets(buff, LEN, f))
+            write(pd[1], buff, LEN);
 
-		fclose(f);
-		close(pd[1]);
+        fclose(f);
+        close(pd[1]);
 
-		wait(NULL);
-	}
+        wait(NULL);
+    }
 
-	return 0;
+    return 0;
 }
