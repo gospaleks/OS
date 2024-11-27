57589a1b74af0ffdc49ce16b5bacf1e8023a0946
bxgda <jovan.bogdanovic@elfak.rs>
2024-11-27 16:43:33 +0100

    ispravka

--- Lab/Lab 4/Zad_5/zad5.c
+++ Lab/Lab 4/Zad_5/zad5.c
@@ -19,83 +19,70 @@ int main()
     if (pipe(neparanZbir) < 0)
         return 1;
 
-    int pidParni = fork();
+    int pid1, pid2;
 
-    if (pidParni == 0)
+    if (pid1 = fork() != 0)
+    {
+        /* roditelj */
+
+        close(paranZbir[0]);
+        close(neparanZbir[0]);
+
+        char niz[N];
+        int suma = 0;
+        for (int i = 0; i < N; i++)
+        {
+            niz[i] = rand() % 95 + 32;
+            suma += niz[i];
+        }
+        printf("%d\n", suma);
+        if (suma % 2 == 0)
+            write(paranZbir[1], niz, sizeof(char) * N);
+        else
+            write(neparanZbir[1], niz, sizeof(char) * N);
+
+        close(paranZbir[1]);
+        close(neparanZbir[1]);
+    }
+    else if (pid2 = fork() != 0)
     {
         /* proces 2 */
 
-        // close(neparanZbir[1]);
-        // close(neparanZbir[0]);
+        close(neparanZbir[0]);
+        close(neparanZbir[1]);
         close(paranZbir[1]);
 
         char niz[N];
-        read(paranZbir[0], niz, sizeof(char) * N);
+        if (read(paranZbir[0], niz, sizeof(char) * N))
+        {
+            FILE *f = fopen("KARAKTERI.txt", "w");
+            for (int i = 0; i < N; i++)
+                fprintf(f, "%c", niz[i]);
+            fprintf(f, "\n");
+            fclose(f);
 
-        FILE *f = fopen("KARAKTERI.txt", "w");
-        for (int i = 0; i < N; i++)
-            fprintf(f, "%c", niz[i]);
-        fprintf(f, "\n");
-        fclose(f);
-
-        close(paranZbir[0]);
-
+            close(paranZbir[0]);
+        }
         exit(0);
     }
     else
     {
-        int pidNeparni = fork();
+        /* proces 3 */
 
-        if (pidNeparni == 0)
+        close(paranZbir[0]);
+        close(paranZbir[1]);
+        close(neparanZbir[1]);
+
+        char niz[N];
+        if (read(neparanZbir[0], niz, sizeof(char) * N))
         {
-            /* proces 3 */
-
-            // close(paranZbir[1]);
-            // close(paranZbir[0]);
-            close(neparanZbir[1]);
-
-            char niz[N];
-            read(neparanZbir[0], niz, sizeof(char) * N);
-
             for (int i = 0; i < N; i++)
                 printf("%c", niz[i]);
             printf("\n");
 
             close(neparanZbir[0]);
-
-            exit(0);
         }
-        else
-        {
-            /* roditelj */
-
-            close(paranZbir[0]);
-            close(neparanZbir[0]);
-
-            char niz[N];
-            int suma = 0;
-            for (int i = 0; i < N; i++)
-            {
-                niz[i] = rand() % 95 + 32;
-                suma += niz[i];
-            }
-            printf("%d\n", suma);
-            if (suma % 2 == 0)
-            {
-                write(paranZbir[1], niz, sizeof(char) * N);
-                kill(pidNeparni, SIGKILL);
-            }
-            else
-            {
-                write(neparanZbir[1], niz, sizeof(char) * N);
-                kill(pidParni, SIGKILL);
-            }
-
-            close(paranZbir[1]);
-            close(neparanZbir[1]);
-
-            wait(NULL);
-        }
+        exit(0);
     }
     return 0;
 }
--- Lab/Lab 4/Zad_9/zad9.c
+++ Lab/Lab 4/Zad_9/zad9.c
@@ -19,77 +19,69 @@ int main()
     if (pipe(neparanZbir) < 0)
         return 1;
 
-    int pidParni = fork();
+    int pid1, pid2;
 
-    if (pidParni == 0)
+    if (pid1 = fork() != 0)
+    {
+        /* roditelj */
+
+        close(paranZbir[0]);
+        close(neparanZbir[0]);
+
+        int niz[N];
+        int suma = 0;
+        for (int i = 0; i < N; i++)
+        {
+            niz[i] = rand() % 20;
+            suma += niz[i];
+        }
+
+        if (suma % 2 == 0)
+            write(paranZbir[1], niz, sizeof(int) * N);
+        else
+            write(neparanZbir[1], niz, sizeof(int) * N);
+
+        close(paranZbir[1]);
+        close(neparanZbir[1]);
+    }
+    else if (pid2 = fork() != 0)
     {
         /* dete1 - roditelj drugog deteta (proces2) */
 
+        close(neparanZbir[0]);
+        close(neparanZbir[1]);
         close(paranZbir[1]);
 
         int niz[N];
-        read(paranZbir[0], niz, sizeof(int) * N);
+        if (read(paranZbir[0], niz, sizeof(int) * N))
+        {
+            FILE *f = fopen("BROJEVI.txt", "w");
+            for (int i = 0; i < N; i++)
+                fprintf(f, "%d\n", niz[i]);
+            fclose(f);
 
-        FILE *f = fopen("BROJEVI.txt", "w");
-        for (int i = 0; i < N; i++)
-            fprintf(f, "%d\n", niz[i]);
-        fclose(f);
-
-        close(paranZbir[0]);
-
+            close(paranZbir[0]);
+        }
         exit(0);
     }
     else
     {
-        int pidNeparni = fork();
-        if (pidNeparni == 0)
+        /* dete (proces3)*/
+
+        close(paranZbir[0]);
+        close(paranZbir[0]);
+        close(neparanZbir[1]);
+
+        int niz[N];
+        if (read(neparanZbir[0], niz, sizeof(int) * N))
         {
-            /* dete (proces3)*/
-
-            close(neparanZbir[1]);
-
-            int niz[N];
-            read(neparanZbir[0], niz, sizeof(int) * N);
-
             for (int i = 0; i < N; i++)
                 printf("%d\n", niz[i]);
 
             close(neparanZbir[0]);
-
-            exit(0);
         }
 
-        else
-        {
-            /* roditelj svih (deda) */
-
-            close(paranZbir[0]);
-            close(neparanZbir[0]);
-
-            int niz[N];
-            int suma = 0;
-            for (int i = 0; i < N; i++)
-            {
-                niz[i] = rand() % 20;
-                suma += niz[i];
-            }
-
-            if (suma % 2 == 0)
-            {
-                write(paranZbir[1], niz, sizeof(int) * N);
-                kill(pidNeparni, SIGKILL);
-            }
-            else
-            {
-                write(neparanZbir[1], niz, sizeof(int) * N);
-                kill(pidParni, SIGKILL);
-            }
-
-            close(paranZbir[1]);
-            close(neparanZbir[1]);
-
-            wait(NULL);
-        }
-        return 0;
+        exit(0);
     }
+    return 0;
 }
